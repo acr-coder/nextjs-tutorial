1a6e1c30a7209da523ed5030700a941b27c42f74
acr-coder <acarerisa1@gmail.com>
2021-06-14 00:27:30 +0300

    lesson6

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 
 
-export default function Home() {
+export default function Home({yorumlar}) {
   return (
     <div>
       <Head>
@@ -9,6 +9,30 @@ export default function Home() {
         <meta name='keywords' content="programming, web, development" />
       </Head>
       <h1>Homepage</h1>
+      {
+        yorumlar.map(yorum => {
+          return(
+            <div key={yorum.id}>
+            <h3>{yorum.name}</h3>
+            <h6>{yorum.body}</h6>
+            </div>
+          )
+        })
+      }
+     
     </div>
   )
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=10')
+  const yorumlar = await res.json()
+
+  return {
+    props:{
+      yorumlar
+    }
+  }
+
+}
+
